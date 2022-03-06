e0a6c51515baaf866f241b708952910ee8936b4d
purva patel <19mca065@nirmauni.ac.in>
2022-03-06 16:20:28 +0530

    homework week 2 3

--- week 1/angular/my-app/src/app/app.module.ts
+++ week 1/angular/my-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CrudHttpService } from './crud-http.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
